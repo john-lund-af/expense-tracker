6233a9954611180c696695ce665a77cdaee71d8c
John Lundgren <john.lund.af@gmail.com>
2024-03-10 08:33:52 +0100

    Added react-icons

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,8 +1,10 @@
+import { RiMoneyDollarCircleFill } from "react-icons/ri";
 
 const Navbar = () => {
   return (
     <nav className="navbar bg-primary text-light p-3">
       <p className="h1">Expense Tracker</p>
+      <RiMoneyDollarCircleFill size={32} />
     </nav>
   )
 }
