01fa99f8f123c52007a9ed147cc96300a1291f1d
John Lundgren <john.lund.af@gmail.com>
2024-03-13 05:55:42 +0100

    Added validation to category select in form

--- src/Form.tsx
+++ src/Form.tsx
@@ -8,9 +8,9 @@ interface Props {
 }
 
 const schema = z.object({
-  description: z.string().min(3, { message: 'Description must be at least 3 characters.' }),
+  description: z.string().min(3, { message: 'Description must be at least 3 characters' }),
   amount: z.number({ invalid_type_error: 'Amount field is required' }),
-  category: z.string()
+  category: z.enum(['groceries', 'utilities', 'entertainment'])
 });
 
 type FormData = z.infer<typeof schema>
@@ -54,8 +54,10 @@ const Form = ({ handleExpense }: Props) => {
       <div className="mb-3">
         <label htmlFor="category" className="form-label">Category</label>
         <select {...register('category')} name="category" id="category" className="form-select">
+          <option value=""></option>
           {categories.map(category => <option key={category} value={category}>{capitalize(category)}</option>)}
         </select>
+        {errors.category && <div className="alert alert-danger mt-2">Choose an existing category</div>}
       </div>
       <button className="btn btn-primary" type="submit">Submit</button>
     </form>
